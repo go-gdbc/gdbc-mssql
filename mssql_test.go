6983b914991866d168fbb373f528d4e5d0ab7eda
Burak Köken <burakkokenn@gmail.com>
2021-01-23 20:55:03 +0200

    Renamed the unit test method's names.

--- mssql_test.go
+++ mssql_test.go
@@ -20,7 +20,7 @@ func getDSNWithUser(t *testing.T, dataSourceUrl string, username string, passwor
 	return adapter.GetDataSourceName(dataSource)
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithPort(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithPort(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username:password@localhost:3000?param1=value&param2=value")
 	assert.Nil(t, err)
 	assert.Contains(t, dsn, "sqlserver://username:password@localhost:3000")
@@ -28,7 +28,7 @@ func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithPort(t *testing.T) {
 	assert.Contains(t, dsn, "param2=value")
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithPortAndWithoutPassword(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithPortAndWithoutPassword(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username@localhost:3000?param1=value&param2=value")
 	assert.Nil(t, err)
 	assert.Contains(t, dsn, "sqlserver://username@localhost:3000")
@@ -36,7 +36,7 @@ func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithPortAndWithoutPassword(
 	assert.Contains(t, dsn, "param2=value")
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithInstance(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithInstance(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username:password@localhost/instanceName?param1=value&param2=value")
 	assert.Nil(t, err)
 	assert.Contains(t, dsn, "sqlserver://username:password@localhost/instanceName")
@@ -44,14 +44,14 @@ func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithInstance(t *testing.T) 
 	assert.Contains(t, dsn, "param2=value")
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithWrongInstance(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithWrongInstance(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username:password@localhost/instanceName/test?param1=value&param2=value")
 	assert.NotNil(t, err)
 	assert.Equal(t, "instance format is wrong : instanceName/test", err.Error())
 	assert.Empty(t, dsn)
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithInstanceAndWithoutPassword(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithInstanceAndWithoutPassword(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username@localhost/instanceName?param1=value&param2=value")
 	assert.Nil(t, err)
 	assert.Contains(t, dsn, "sqlserver://username@localhost/instanceName")
@@ -59,42 +59,48 @@ func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithInstanceAndWithoutPassw
 	assert.Contains(t, dsn, "param2=value")
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithPortAndWithoutArguments(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithPortAndWithoutArguments(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username:password@localhost:3000")
 	assert.Nil(t, err)
 	assert.Equal(t, dsn, "sqlserver://username:password@localhost:3000")
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithInstancePortAndWithoutArguments(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithInstancePortAndWithoutArguments(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username@localhost/instanceName")
 	assert.Nil(t, err)
 	assert.Equal(t, dsn, "sqlserver://username@localhost/instanceName")
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutInstanceAndPort(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithoutInstanceAndPort(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username@localhost")
 	assert.NotNil(t, err)
 	assert.Equal(t, "you need specify port or instance", err.Error())
 	assert.Empty(t, dsn)
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithInstanceAndPort(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithInstanceAndPort(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://username@localhost:3000/instance")
 	assert.NotNil(t, err)
 	assert.Equal(t, "you need only specify port or instance", err.Error())
 	assert.Empty(t, dsn)
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutHost(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithoutHost(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://")
 	assert.NotNil(t, err)
 	assert.Equal(t, "host cannot be empty", err.Error())
 	assert.Empty(t, dsn)
 }
 
-func TestMySQLDataSourceNameAdapter_GetDataSourceNameWithoutUser(t *testing.T) {
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithoutUser(t *testing.T) {
 	dsn, err := getDSN(t, "gdbc:sqlserver://localhost:3000")
 	assert.NotNil(t, err)
 	assert.Equal(t, "user cannot be empty", err.Error())
 	assert.Empty(t, dsn)
+}
+
+func TestMsSQLDataSourceNameAdapter_GetDataSourceNameWithUserAndPassword(t *testing.T) {
+	dsn, err := getDSNWithUser(t, "gdbc:sqlserver://localhost:3000", "username", "password")
+	assert.Nil(t, err)
+	assert.Equal(t, dsn, "sqlserver://username:password@localhost:3000")
 }
